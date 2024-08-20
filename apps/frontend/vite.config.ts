3f941a3619034b7498983853b2c61c63bf3b44c0
Benjamin Chiang <chiangbenjamin991@gmail.com>
2024-08-20 15:27:59 -0700

    feat(frontend): prepare for shadcn

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { join } from 'path';
+import { join, resolve } from 'path';
 
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
@@ -14,6 +14,11 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
     envDir,
     server: {
       host: HOST,
