6559c37440ee37a8c19735ddecce9318e1b3a211
Benjamin Chiang <chiangbenjamin991@gmail.com>
2024-09-03 15:03:13 -0700

    feat(frontend): create placeholder dashboard page

---
+++ apps/frontend/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+function DashboardPage() {
+  return (
+    <main className="min-h-screen bg-background">
+      <div className="flex flex-col items-center justify-center mt-32 text-foreground">
+        <h1 className="text-2xl font-bold">Under Construction!</h1>
+      </div>
+    </main>
+  );
+}
+
+export default DashboardPage;
